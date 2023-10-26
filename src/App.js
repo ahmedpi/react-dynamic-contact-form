5c26138e9009a334d21e43e1f550fe2d25efbf72
ahmedpi <ahmedali.pisa@gmail.com>
2023-10-26 10:39:14 +0200

    dynamic form using JSON

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
 import './App.css';
 import { Card, CardContent, Typography, Grid, TextField, Button } from '@mui/material';
+import { inputFromElement } from './FormElement'
 
 function App() {
+  const margin = { margin: "0 5px" };
   return (
     <div className="App">
       <Typography gutterBottom variant='h3' align="center">
         React-App
       </Typography>
 
-      <Card style={{maxWidth:450, margin: "0 auto", padding:"20px 5px"}}>
+      <Card style={{ maxWidth: 450, margin: "0 auto", padding: "5px 5px" }}>
         <CardContent>
           <Typography gutterBottom variant="h5">Contact Us</Typography>
           <Typography variant="body2" component="p" color="textSecondary">Fill up the form and our team will get back to you with 24 hours.</Typography>
           <form>
-          <Grid container spacing={1}>
-            <Grid item xs={12} sm={6}>
-              <TextField label="First Name" placeholder="Enter first name" variant="outlined" fullWidth required />
+            <Typography variant="body2" align="left" gutterBottom>Personal Info:</Typography>
+            <Grid container spacing={1}>
+              {
+                inputFromElement.slice(0, 4).map(input =>
+                  <Grid item xs={input.xs} sm={input.s}>
+                    <TextField {...input} />
+                  </Grid>
+                )
+              }
             </Grid>
-            <Grid xs={12} sm={6} item>
-              <TextField label="Last Name" placeholder="Enter last name" variant="outlined" fullWidth required />
+
+            <Typography variant="body2" align="left" gutterBottom>Address:</Typography>
+            <Grid container spacing={1}>
+              {
+                inputFromElement.slice(4, 9).map(input =>
+                  <Grid item xs={input.xs} sm={input.s}>
+                    <TextField {...input} />
+                  </Grid>
+                )
+              }
+              <Grid xs={12} item>
+                <TextField multiline rows={3} label="Message" placeholder="Type your message" variant="outlined" fullWidth required />
+              </Grid>
             </Grid>
-            <Grid xs={12} item>
-              <TextField type="email" label="Email" placeholder="Enter email" variant="outlined" fullWidth required />
+            <Grid container spacing={1} marginTop="5px">
+              <Grid xs={12} item align="right">
+                <Button style={margin} type="rest" variant="outlined" color="primary">Reset</Button>
+                <Button type="submit" variant="contained" color="primary">Submit</Button>
+              </Grid>
             </Grid>
-            <Grid xs={12} item>
-              <TextField type="number" label="Phone" placeholder="Enter phone number" variant="outlined" fullWidth required />
-            </Grid>
-            <Grid xs={12} item>
-              <TextField multiline rows={4} label="Message" placeholder="Type your message" variant="outlined" fullWidth required />
-            </Grid>
-            <Grid xs={12} item>
-              <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
-            </Grid>
-          </Grid>
           </form>
         </CardContent>
       </Card>
---
+++ src/FormElement.js
@@ -0,0 +1,94 @@
+export const inputFromElement = [
+    {
+        name: "firstName",
+        placeholder: "Enter first name",
+        label: "First Name", 
+        variant: "outlined", 
+        fullWidth: true, 
+        required: true,
+        xs:12,
+        s:6
+    },
+    {
+        name: "lastName",
+        placeholder: "Enter last name",
+        label: "Last Name", 
+        variant: "outlined", 
+        fullWidth: true, 
+        required: true,
+        xs:12,
+        s:6
+    },
+    {
+        name: "email",
+        type:"email",
+        placeholder: "Enter email",
+        label: "Email", 
+        variant: "outlined", 
+        fullWidth: true, 
+        required: true,
+        xs:12,
+        s:6
+    },
+    {
+        name: "phone",
+        type:"number",
+        placeholder: "Enter phone number",
+        label: "Phone", 
+        variant: "outlined", 
+        fullWidth: true, 
+        required: true,
+        xs:12,
+        s:6
+    },
+    {
+        name: "street",
+        placeholder: "Enter street",
+        label: "Street", 
+        variant: "outlined", 
+        fullWidth: true, 
+        required: true,
+        xs:12,
+        s:6
+    },
+    {
+        name: "city",
+        placeholder: "Enter city",
+        label: "City", 
+        variant: "outlined", 
+        fullWidth: true, 
+        required: true,
+        xs:12,
+        s:6
+    },
+    {
+        name: "state",
+        placeholder: "Enter state",
+        label: "State", 
+        variant: "outlined", 
+        fullWidth: true, 
+        required: true,
+        xs:12,
+        s:6
+    },
+    {
+        name: "country",
+        placeholder: "Enter country",
+        label: "Country", 
+        variant: "outlined", 
+        fullWidth: true, 
+        required: true,
+        xs:12,
+        s:6
+    },
+    {
+        name: "postalCode",
+        placeholder: "Enter postal code",
+        label: "Postal code", 
+        variant: "outlined", 
+        fullWidth: true, 
+        required: true,
+        xs:12,
+        s:6
+    }
+]
